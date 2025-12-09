b3bb59c60a6feabfe65e8828e6b790181a0b4b8c
Starrbr1ght <alaynabentley3@gmail.com>
2025-12-09 09:07:16 -0600

    Enhance loops lesson with examples and explanations
    Added examples of loop iterations and credit card printing.

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,24 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+#you can iterate over a range, string, sequence, etc.
+for x in range (1, 11, 2):
+    print(x)
+
+
+for x in reversed(range(1,11)):
+    print(x)
+print("happy new year!")
+
+for x in range (1, 11, 2):
+    print(x) #counts by 2
+credit_card= 1234-5678-9012-3456
+
+for x in credit_card: 
+    print(x)
+
+
+for x in range (1, 21): # ends when you hit 12 so then x doesnt become 13
+    if x==13:
+        continue
+    else:
+        print(x)
