6e4109013f8e18ca3772d399bc2decd68082b5d5
Starrbr1ght <alaynabentley3@gmail.com>
2025-12-09 09:06:49 -0600

    Update 1_loops.py

--- 1_loops.py
+++ 1_loops.py
@@ -15,13 +15,30 @@ for fruit in fruits:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
     print(subject)
+#print out each subject but stop when you reach histroy
+for subject in  (subjects):
+    if subject == "History":
+        break
+    else:
+        print(subject)
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
-
-
+for i in range (len(subjects)):
+    print(f"subject {i}: {subjects[i]}")
+#output: assigns each class with "subject" and then a number.
 # Given:
 numbers = [5, 10, 15, 20]
-
+total=0
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+for number in numbers:
+    total += number
+print(total)
+
+#first time total = 0
+# second time total= 0+5
+#third time total= 5+10
+#fourth time total = 5 + 10
+#fourth time total = 15 + 15
+# fifth time total = 30 + 20
